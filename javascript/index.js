e303169c2d2f8a50def5f525bf921fff9d1f7286
Vikash <vksingh2801@gmail.com>
2022-10-01 23:33:33 +0530

    updated error handling scenario

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,42 @@
-let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach( (button) =>{
-    button.addEventListener('click', (e)=>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            document.querySelector('input').value = string;
-        }
-        else if(e.target.innerHTML == 'C'){
-            string = "";
-            document.querySelector('input').value = string;
-        }
-        else if(e.target.innerHTML == 'B'){
-            string = string.slice(0, -1);
-            document.querySelector('input').value = string;
-        }
-        else{
-            console.log(e.target);
-            string = string + e.target.innerHTML;
-            document.querySelector('input').value = string;
-        }
+function myCalc(){
+    let string = "";
+    let buttons = document.querySelectorAll('.button');
+    Array.from(buttons).forEach( (button) =>{
+        
+            button.addEventListener('click', (e)=>{
+                if(e.target.innerHTML == '='){
+                    
+                    try{
+                        string = eval(string);
+                        document.querySelector('input').value = string;
+                    }
+                    catch(err){
+                        document.querySelector('input').value = 'INVALID OPERATION';
+                    }
+
+                }
+                else if(e.target.innerHTML == 'Clr'){
+                    string = "";
+                    document.querySelector('input').value = string;
+                }
+                else if(e.target.innerHTML == 'Del'){
+                    string = string.slice(0, -1);
+                    document.querySelector('input').value = string;
+                }
+                else{
+                    console.log(e.target);
+                    if(document.querySelector('input').value == 'INVALID OPERATION'){
+                        string = e.target.innerHTML;    
+                    }
+                    else{
+                        string = string + e.target.innerHTML;
+                    }
+                    document.querySelector('input').value = string;
+                    
+                }
+            })  
+       
     })
-})
+
+}
+
